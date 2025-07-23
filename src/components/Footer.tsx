4619af85fe6c15ce77291034acbc49bc77a3df1a
Wil-byte <projectwrite0@gmail.com>
2025-07-23 01:30:20 +0100

    Setup for GitHub Pages deployment

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import '../assets/styles/Footer.scss'
+
+function Footer() {
+  return (
+    <footer>
+      <div>
+        <p>Jst_wil's Portfolio </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
